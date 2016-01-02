37be3a96a3fac28b21a030a2c1a794add35ee809
Alexander Kurakin <kuraga333@mail.ru>
2016-01-02 15:50:15 +0300

    Update index.js

--- test/index.js
+++ test/index.js
@@ -66,6 +66,18 @@ describe('route-trie', function () {
     assert.notStrictEqual(node, trie.define('/test::name(a|b)'))
   })
 
+  it('twice error case', function () {
+    trie = new Trie()
+
+    try {
+      trie.define('///');
+    } catch (e) {}
+
+    assert.throws(function () {
+      trie.define('///')
+    }, null, 'Multi-slash exist.')
+  });
+
   it('trie.match', function () {
     var trie = new Trie()
 
